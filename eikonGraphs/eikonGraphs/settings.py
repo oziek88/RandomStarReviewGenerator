b960dc1297c6aec0a564de5f2c754016bee0f568
Ian <oziek88@gmail.com>
2023-03-22 21:41:55 -0400

    Update settings.py

--- eikonGraphs/eikonGraphs/settings.py
+++ eikonGraphs/eikonGraphs/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yaby0t=rq5(ld0)ln5gt7+n7awyzw5yn)-i07nk$w=#t(vfb7v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
